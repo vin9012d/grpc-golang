a1daf2366f73498380a017f51ea782fe2b997013
Vinodbhai <vinodbhaichaudhari@Vinodbhai-Chaudhari.local>
2025-10-06 09:20:40 +0530

    server client side boiperplate code setup done

---
+++ greet/server/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net"
+
+	pb "example.com/greet/proto"
+	"google.golang.org/grpc"
+)
+
+
+var addr string = "0.0.0.0:50051";
+
+type Server struct{
+pb.GreetServiceServer
+}
+
+func main() {
+ fmt.Println("Starting gRPC server..."+ addr)
+	lis, err := net.Listen("tcp", addr);
+	
+	if err != nil {
+		 fmt.Println("Failed to listen:", err)
+	}
+
+	println("Server listening at", addr)
+	s:= grpc.NewServer();
+
+	if err = s.Serve(lis); err != nil {
+		 fmt.Println("Failed to serve:", err)
+	}
+
+	
+
+}
